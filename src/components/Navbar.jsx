bfcd4e201010b39676dd38dad61a17e28d7a3cba
Abhijeet Gupta <abhijeet.sci02@gmail.com>
2026-04-05 19:03:51 +0530

    NAV-BAR COMPLETED

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,15 +2,38 @@ function Navbar()
 {
     return (
         <>
-        <div className="full_navbar h-20 var(--bg) --text">
+        <div className="full_navbar h-20 w-full flex items-center justify-between px-6"style={{backgroundColor:'#f4f7fe'}}>
 
-            <div className="name_navbar w-11 bg-var(--accent)">ghi</div>
+            {/* NAME LOGO */}
+            <div className="name_navbar ml-12  text-3xl font-bold  relative flex flex-wrap  ">
+                <span className=" text-[var(--secondary-color)] font-[var(--Winky-font)]">Abhi</span>
+                <span className=" text-[var(--primary-color)] font-[var(--Winky-font)]">jeet</span>
+            </div>
+            
+            {/*MENU SECTION */}
+
+            {/* This section includes The navbar elements such as Skills , projects , etc*/}
+            <div className="element_navbar hidden md:flex font-[var(--Josefin-font)] text-3xl gap-10 px-4">
+                <a className=""style={{color:'#fca61f'}}href="">Home</a>
+                <a className=""href="">Skills  </a>
+                <a className=""href="">Projects</a>
+                <a className=""href="">Contact</a>
+            </div>
 
 
+            {/* Contact Button in Big Formate */}
+            <div className="contact_button_navbar hidden md:block ">
+                <button className="bg-[#fca61f] px-4 py-5 rounded-lg ">CONTACT ME</button>
+            </div>
+        
+
+            {/*MOBILE MENU ICON */}
+            <div className="md:hidden">
+                <button>☰</button>
+            </div>
+        
         </div>
-        
-        
         </>
-    )
+    );
 }
 export default Navbar
