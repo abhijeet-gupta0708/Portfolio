8a1e65427bf0c9ff9bc69bc8fe23caef649f1093
Abhijeet Gupta <abhijeet.sci02@gmail.com>
2026-04-05 11:19:07 +0530

    Initial commit

---
+++ Portfolio/src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+function Navbar()
+{
+    return (
+        <>
+        <div className="full_navbar h-20 var(--bg) --text">
+
+            <div className="name_navbar w-11 bg-var(--accent)">ghi</div>
+
+
+        </div>
+        
+        
+        </>
+    )
+}
+export default Navbar
